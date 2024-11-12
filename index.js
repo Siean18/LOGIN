6958fda2fb559bf80994c4d0b78b41de8008a663
Raihan Rizky <siean180@gmail.com>
2024-11-13 00:07:17 +0700

    menambahkan animasi

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 function toggleDarkMode() {
-    const toggleButtons = document.getElementsByClassName("card");
-    const toggleButtons2 = document.getElementsByClassName("card-moon");
+    const toggleButtons = document.getElementById("sun");
+    const toggleButtons2 = document.getElementById("moon");
     
-    if (toggleButtons.length > 0) {
-        toggleButtons[0].style.display = "none";
-        toggleButtons2[0].style.display = "block";
+    if (toggleButtons && toggleButtons2) {
+        toggleButtons.style.animation = "sunToBottom 0.5s forwards";
+        toggleButtons2.style.animation = "moonToTop 0.5s forwards";
+
+        setTimeout(() => {
+            toggleButtons.style.display = "none"; 
+            toggleButtons2.style.display = "block";
+        }, 500);
         document.body.style.background = "linear-gradient(45deg, #526a6d, #484e6d)";
     }
 }
 
 function toggleLightMode() {
-    const toggleButtons = document.getElementsByClassName("card");
-    const toggleButtons2 = document.getElementsByClassName("card-moon");
+    const toggleButtons = document.getElementById("sun");
+    const toggleButtons2 = document.getElementById("moon");
     
-    if (toggleButtons.length > 0) {
-        toggleButtons[0].style.display = "block";
-        toggleButtons2[0].style.display = "none";
+    if (toggleButtons && toggleButtons2) {
+        toggleButtons2.style.animation = "sunToBottom 0.5s forwards";
+        toggleButtons.style.animation = "moonToTop 0.5s forwards";
+        setTimeout(() => {
+            toggleButtons2.style.display = "none";
+            toggleButtons.style.display = "block";
+        }, 500);
         document.body.style.background = "linear-gradient(45deg, #59a9b3, #5970f2)";
     }
 }
